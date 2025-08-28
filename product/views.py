16c34198b3f046c6049f632f373d678cbfd6fbb9
Nitheesh Devarajan <nitheeshdevarajan9988@gmail.com>
2025-08-28 10:39:22 +0530

    Add files via upload

---
+++ product/views.py
@@ -0,0 +1,174 @@
+from django.shortcuts import render,redirect
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate,login
+from . models import Customer
+from django.contrib import messages
+from django.http import JsonResponse
+import json
+import datetime
+from .models import * 
+from .utils import cookieCart, cartData, guestOrder
+
+def store(request):
+	data = cartData(request)
+
+	cartItems = data['cartItems']
+	order = data['order']
+	items = data['items']
+
+	products = Product.objects.all()
+	context = {'products':products, 'cartItems':cartItems}
+	return render(request, 'store/store.html', context)
+
+
+def cart(request):
+	data = cartData(request)
+
+	cartItems = data['cartItems']
+	order = data['order']
+	items = data['items']
+
+	context = {'items':items, 'order':order, 'cartItems':cartItems}
+	return render(request, 'store/cart.html', context)
+
+def checkout(request):
+	data = cartData(request)
+	
+	cartItems = data['cartItems']
+	order = data['order']
+	items = data['items']
+
+	context = {'items':items, 'order':order, 'cartItems':cartItems}
+	return render(request, 'store/checkout.html', context)
+
+def updateItem(request):
+	data = json.loads(request.body)
+	productId = data['productId']
+	action = data['action']
+	print('Action:', action)
+	print('Product:', productId)
+
+	customer = request.user.customer
+	product = Product.objects.get(id=productId)
+	order, created = Order.objects.get_or_create(customer=customer, complete=False)
+
+	orderItem, created = OrderItem.objects.get_or_create(order=order, product=product)
+
+	if action == 'add':
+		orderItem.quantity = (orderItem.quantity + 1)
+	elif action == 'remove':
+		orderItem.quantity = (orderItem.quantity - 1)
+
+	orderItem.save()
+
+	if orderItem.quantity <= 0:
+		orderItem.delete()
+
+	return JsonResponse('Item was added', safe=False)
+
+def processOrder(request):
+	transaction_id = datetime.datetime.now().timestamp()
+	data = json.loads(request.body)
+
+	if request.user.is_authenticated:
+		customer = request.user.customer
+		order, created = Order.objects.get_or_create(customer=customer, complete=False)
+	else:
+		customer, order = guestOrder(request, data)
+
+	total = float(data['form']['total'])
+	order.transaction_id = transaction_id
+
+	if total == order.get_cart_total:
+		order.complete = True
+	order.save()
+
+	if order.shipping == True:
+		ShippingAddress.objects.create(
+		customer=customer,
+		order=order,
+		address=data['shipping']['address'],
+		city=data['shipping']['city'],
+		state=data['shipping']['state'],
+		zipcode=data['shipping']['zipcode'],
+		)
+
+	return JsonResponse('Payment submitted..', safe=False)
+
+def product_description(request):
+	return render(request, 'store/product_description.html')	
+
+
+def menu(request):
+	return render(request, 'store/menu.html')	
+
+def product_details(request,pk):
+	product=Product.objects.get(pk=pk)
+	context={'product':product}
+	return render(request, 'store/product_details.html',context)
+
+
+
+def products_list(request):
+	return render(request, 'store/products_list.html')	
+
+
+def products(request):
+	return render(request, 'store/products.html')	
+
+def blank_layout(request):
+	return render(request, 'store/blank_layout.html')
+
+
+def account(request):
+
+	
+		
+	if request.POST and 'register' in request.POST:
+		try:
+
+			username=request.POST.get('username')
+			password=request.POST.get('password')
+			email=request.POST.get('email')
+			address=request.POST.get('address')
+			phone=request.POST.get('phone')	
+
+			user=User.objects.create_user(
+				username=username,
+				password=password,
+				email=email
+			)
+
+			customer=Customer.objects.create(
+				user=user,
+				phone=phone,
+				address=address
+			)
+			success_message="User registred successfully"
+			messages.success(request,success_message)
+		except Exception as e:
+			error_message="Duplicate username or invalid inputs"
+			messages.error(request,error_message)
+	if request.POST and 'login' in request.POST:
+		username=request.POST.get('username')
+		password=request.POST.get('password')
+		user=authenticate(username=username,password=password)
+		if user:
+			login(request,user)
+			return redirect('home')
+		else:
+			messages.error(request,'invalid user credentials')
+						
+
+
+		
+
+
+	return render(request, 'store/login.html')	
+
+def watches(request):
+	return render(request, 'store/watch.html')
+
+
+	
+			
